6fce6aa168be36479fe73630b7962b21d9cd61f6
Paras14 <53565432+Paras14@users.noreply.github.com>
2024-08-15 22:00:26 +0200

    Add Order service functionality with test case

---
+++ order-service/src/main/java/com/practice/project/order_service/service/OrderService.java
@@ -0,0 +1,28 @@
+package com.practice.project.order_service.service;
+
+import com.practice.project.order_service.dto.OrderRequest;
+import com.practice.project.order_service.model.Order;
+import com.practice.project.order_service.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest){
+
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setPrice(orderRequest.price());
+        order.setSkuCode(orderRequest.skuCode());
+        order.setQuantity(orderRequest.quantity());
+
+        orderRepository.save(order);
+    }
+
+}
--- order-service/src/test/java/com/practice/project/order_service/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/practice/project/order_service/OrderServiceApplicationTests.java
@@ -1,15 +1,60 @@
 package com.practice.project.order_service;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MySQLContainer;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrderServiceApplicationTests {
 
+	@ServiceConnection
+	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
+
+	@LocalServerPort
+	private int port;
+
+	@BeforeEach
+	void setUp() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mySQLContainer.start();
+	}
+
 	@Test
-	void contextLoads() {
+	void supposedToPlaceOrder() {
+		String submitOrderJson = """
+				{
+				    "skuCode": "iphone_13",
+				    "price": 400,
+				    "quantity": 1
+				}
+				""";
+
+		var responseBodyString = RestAssured.given()
+				.contentType("application/json")
+				.body(submitOrderJson)
+				.when()
+				.post("/api/order")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.extract()
+				.body().asString();
+
+		assertThat(responseBodyString, Matchers.is("Order placed successfully!"));
+
 	}
 
 }
