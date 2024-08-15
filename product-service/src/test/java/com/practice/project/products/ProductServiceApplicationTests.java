175ea5ec6238effcbcec2093bd3aedc558a4d56c
Paras14 <53565432+Paras14@users.noreply.github.com>
2024-08-15 17:17:35 +0200

    Add test case for testing product service

--- product-service/src/test/java/com/practice/project/products/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/practice/project/products/ProductServiceApplicationTests.java
@@ -1,15 +1,56 @@
 package com.practice.project.products;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
+	@ServiceConnection
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setUp() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mongoDBContainer.start();
+	}
+
 	@Test
-	void contextLoads() {
+	void supposedToCreateProduct() {
+		String requestBody = """
+				{
+				    "name": "iPhone 15",
+				    "description": "128GB storage 6GB RAM 48MP camera A16 bionic chip",
+				    "price": 800
+				}				
+				""";
+
+		RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/api/product")
+				.then()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("name", Matchers.equalTo("iPhone 15"))
+				.body("description", Matchers.equalTo("128GB storage 6GB RAM 48MP camera A16 bionic chip"))
+				.body("price", Matchers.equalTo(800));
 	}
 
 }
