c1af1bc0056b2ba69b65c67a7c4be59d1a18c6b2
Paras14 <53565432+Paras14@users.noreply.github.com>
2024-08-17 15:22:34 +0200

    Add Intercommunication b/w order and inventory service, also test case with wiremock

---
+++ inventory-service/src/main/resources/db/migration/V3__add_iphone_inventory.sql
@@ -0,0 +1,2 @@
+insert into t_inventory (quantity, sku_code)
+values (5,'iphone_15');
--- order-service/src/main/java/com/practice/project/order_service/service/OrderService.java
+++ order-service/src/main/java/com/practice/project/order_service/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.practice.project.order_service.service;
 
+import com.practice.project.order_service.client.InventoryClient;
 import com.practice.project.order_service.dto.OrderRequest;
 import com.practice.project.order_service.model.Order;
 import com.practice.project.order_service.repository.OrderRepository;
@@ -14,15 +15,23 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
+    private final InventoryClient inventoryClient;
+
     public void placeOrder(OrderRequest orderRequest){
 
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
+        var isStockAvailable =inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
 
-        orderRepository.save(order);
+        if(isStockAvailable){
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Not enough stock for the sku code : " + orderRequest.skuCode() + " and quantity : "+ orderRequest.quantity()+".");
+        }
     }
 
 }
